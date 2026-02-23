5dde7032e855d1a4fed5f30e479883fdb61d7a55
liviaamota <livia.mota11@aluno.ce.gov.br>
2026-02-23 17:54:05 +0000

    pronto

---
+++ 1-bimestre/teste-de-software/projetos/projeto1-sistema-de-validacao-de-pedidos/test/index.test.js
@@ -0,0 +1,36 @@
+const validarPedido = require('../index')
+
+test("Pedido válido deve retornar true", () => {
+    const pedido = {
+        cliente: "Carlos",
+        total: 150,
+        itens: ["Produto A"],
+    }
+     expect(validarPedido(pedido)).toBe(true)
+})
+
+test("Pedido com total 0 deve retornar false", () =>{
+    const pedido ={
+       cliente: "Carlos",
+	   total: 0,
+	   itens: ["Produto A"],
+    }
+    expect(validarPedido(pedido)).toBe(false)
+})
+
+test("Pedido sem cliente deve retornar false", () => {
+    const pedido ={
+       cliente: "",
+	   total: 150,
+	   itens: ["Produto A"],
+    }
+ expect(validarPedido(pedido)).toBe(false)
+})
+
+test("Pedido sem itens deve retornar false", () => {
+    const pedido ={
+       cliente: "Carlos",
+	   total: 150,
+	   itens: [""],
+    }
+})
